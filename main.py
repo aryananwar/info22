df326aab161ea310cfcb859f26c6e15b7b250891
Matthew Nanas <37747855+matthewnanas@users.noreply.github.com>
2022-02-27 03:08:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import wget
 import os
 
 # Open the folder containing lists of all fits locations
-
 print("Downloading all fits")
 
 for listname in os.listdir("allsky_file_list"):
     with open(os.path.join("allsky_file_list", listname), 'r') as f:
         contents = f.read().split("\n")
         for fit in contents:
-            wget.download("https://mdallsky.astro.umd.edu/masn01-archive/" + fit[2:6] + fit[1:], out="fits/" + fit.split("/")[3])
+            try:
+                wget.download("https://mdallsky.astro.umd.edu/masn01-archive/" + fit[2:6] + fit[1:], out="fits/" + fit.split("/")[3]) # Parse each line and structure a valid url to wget
+            except:
+                print("Caught invalid url")
         print(len(contents))
