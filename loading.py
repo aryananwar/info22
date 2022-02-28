01a974a8daef1b3891eee7c8da19d0426f0664f4
unknown <aryananwar221@gmail.com>
2022-02-28 18:49:49 -0500

    Create loading.py

---
+++ loading.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from astropy.visualization import astropy_mpl_style
+
+
+def loads(f):
+    plt.style.use(astropy_mpl_style)
+    file = fits.open(f) 
+
+    img_data = file[0].data
+
+    plt.figure()
+    plt.imshow(img_data, cmap ="gray")
+    plt.colorbar()
+    plt.show()
+
+loads('test.fits')
