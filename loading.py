976b27032710a5fc93d238e2b81d7381ab213805
unknown <aryananwar221@gmail.com>
2022-03-03 18:33:00 -0500

    she said i hate my day job

--- loading.py
+++ loading.py
@@ -2,21 +2,17 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from astropy.visualization import astropy_mpl_style
 
-hdu = fits.PrimaryHDU()
+
 
 def loads(f):
     plt.style.use(astropy_mpl_style)
     file = fits.open(f) 
-
-    arr = file[0].data
-    for i in file:
-        arr+= file[i].data
-    return arr
-
+    img_data = file[0].data
     plt.figure()
+    
     plt.imshow(img_data, cmap ="gray")
     plt.colorbar()
     plt.show()
 
-x = loads('small spider.fits')
-hdu.header
+
+loads('small_spider.fits')
--- save.py
+++ save.py
@@ -4,12 +4,14 @@ from astropy.visualization import astropy_mpl_style
 from astropy.io import fits
 import numpy as np
 
-song = 'https://open.spotify.com/track/4cAgkb0ifwn0FSHGXnr4F6?si=e73fd5d7e49d405c'
+song = 'https://open.spotify.com/track/7JdarpX08FLhmfNZpxqgUO?si=cca5b218096846ff'
 
 
 def save(f):
     file = fits.open(f) 
     img_data = file[0].data
+    print(img_data.shape)
+    img_data = img_data[120:800, 320: 1110]
     norm = (img_data.astype(np.float)-img_data.min())*255.0 / (img_data.max()-img_data.min())
     Image.fromarray(norm.astype(np.uint8)).save('result.png') 
 
