513688a0ce5f8c0f38d0d708655da1fc6d41a370
Matthew Nanas <37747855+matthewnanas@users.noreply.github.com>
2022-03-02 20:48:48 -0500

    gang

--- detection.py
+++ detection.py
@@ -1,22 +1,57 @@
+# Import necessary libraries
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
-from astropy.io import fits
-from astropy.visualization import astropy_mpl_style
-from loading.py import loads
 
-#open fits file
-loads()
-#adjust contrast and shadows/highlights
+# Load image
+img = cv.imread("img.jpg")
 
+# Apply a median filter to remove noise
+median = cv.medianBlur(img, 5)
 
-#opening image and adding contours
-img = cv.imread('img.jpg')
-imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-ret, thresh = cv.threshold(imgray, 127, 255, 0)
-im2, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-data = cv.drawContours(img, contours, -1, (0,255,0), 3)
+# Step 2----------------------------------------------------------------------------------
+# Convert image to RGB and create a 2D array of color values
+image = cv.cvtColor(median, cv.COLOR_BGR2RGB)
+pixels = image.reshape((-1, 3))
+pixels = np.float32(pixels)
 
-#display image for debugging
-plt.imshow(data, interpolation='nearest')
-plt.show()
+# Save time of kmeans segmentation by specifying a limit on iterations
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 100, 0.2)
+
+# Create color clusters
+k = 2
+_, labels, (centers) = cv.kmeans(pixels, k, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+
+# convert back to 8 bit values
+centers = np.uint8(centers)
+labels = labels.flatten()
+
+# convert all pixels to the color of the centroids
+segmented_image = centers[labels.flatten()]
+segmented_image = segmented_image.reshape(image.shape)
+
+# Step 3----------------------------------------------------------------------------------
+# Setup SimpleBlobDetector parameters.
+params = cv.SimpleBlobDetector_Params()
+
+params.minThreshold = 10;
+params.maxThreshold = 500;
+params.filterByArea = True
+params.minArea = 100
+params.filterByCircularity = True
+params.minCircularity = 0.1
+params.filterByConvexity = True
+params.minConvexity = 0.87
+params.filterByInertia = True
+params.minInertiaRatio = 0.01
+
+detector = cv.SimpleBlobDetector_create(params)
+keypoints = detector.detect(segmented_image)
+
+result = cv.drawKeypoints(segmented_image, keypoints, np.array([]), (0,0,255), cv.DrawMatchesFlags_DRAW_RICH_KEYPOINTS)
+
+print("Number of obstructions detected: " + str(len(keypoints)))
+
+# Display results
+compare = np.concatenate((img, result), axis=1)
+cv.imshow("Sample", compare)
+cv.waitKey(0)
--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # Import libraries
 import wget
 import os
+import matplotlib.pyplot as plt
+from astropy.visualization import astropy_mpl_style
+from astropy.utils.data import get_pkg_data_filename
+from astropy.io import fits
+astropy_mpl_style['axes.grid'] = False
+plt.style.use(astropy_mpl_style)
 
 def init():
     downloadChoice = input("Would you like to download all fits files? (y/n): ")
     if downloadChoice.capitalize() == "Y":
         downloadFits()
     else:
-        print("Analyzing images")
+        anaylizeTest()
 
 # Open the folder containing lists of all fits locations
 def downloadFits():
@@ -20,5 +26,16 @@ def downloadFits():
                     wget.download("https://mdallsky.astro.umd.edu/masn01-archive/" + fit[2:6] + fit[1:], out="fits/" + fit.split("/")[3]) # Parse each line and structure a valid url to wget
                 except Exception as e:
                     print(e)
+
+def anaylizeTest():
+    image_file = get_pkg_data_filename('MASN01-2015-06-19T23-43-22.fits')
+    image_data = fits.getdata(image_file, ext=0)
+    print(image_data)
+    print(fits.info(image_file))
+    plt.figure()
+    plt.imshow(image_data, cmap="gray")
+    plt.colorbar()
+    plt.show()
+
 
 init()
