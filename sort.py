0b052ff0e5b9f0f979739e6da068a09efab85070
Rohit Sharma <kprsharma21@gmail.com>
2022-03-03 19:40:03 -0500

    organize and get data functions

--- sort.py
+++ sort.py
@@ -3,61 +3,70 @@ import os
 import datetime
 dates = {}
 
-for file in os.listdir('fits'):
-    filename = file
-    if 'tmp' not in file and '.fits' in file:
-        if file == '.DS_Store':
-            continue          
-        file = fits.open(f"./fits/{file}")
-        date = file[0].header['DATE-OBS'].split('T')[0]
-        if date not in dates:
-            dates[date] = {
-                "pictures": 1,
-                "telescopeUsed": file[0].header['TELESCOP'],
-                "dates": [file[0].header['DATE-OBS']]
-            }
-            try:
-                os.mkdir(f"./fits/{date}")
-            except:
-                pass
-            os.rename(f"./fits/{filename}", f"./fits/{date}/{file[0].header['DATE-OBS']}.fits")
-        else:
-            dates[date]['pictures'] += 1
-            dates[date]['dates'].append(file[0].header['DATE-OBS'])
-            os.rename(f"./fits/{filename}", f"./fits/{date}/{file[0].header['DATE-OBS']}.fits")
-            if(dates[date]['telescopeUsed'] != file[0].header['TELESCOP']):
-                print('telescope mismatch')
-        
-        '''
-        date = file[0].header['DATE-LOC'].split('T')[0]
+def organize():
+    for file in os.listdir('fits'):
+        filename = file
+        if 'tmp' not in file and '.fits' in file:
+            if file == '.DS_Store':
+                continue
+            file = fits.open(f"./fits/{file}")
+            date = file[0].header['DATE-OBS'].split('T')[0]
+            if date not in os.listdir('fits'):
+                try:
+                    os.mkdir(f"./fits/{date}")
+                except Exception as e:
+                    print(e)
+                    pass
+                os.rename(f"./fits/{filename}", f"./fits/{date}/{file[0].header['DATE-OBS']}.fits")
+            else:   
+                os.rename(f"./fits/{filename}", f"./fits/{date}/{file[0].header['DATE-OBS']}.fits")
+            
 
-        print(file[0].header['DATE-LOC'].split('T')[0])
-        if(file[0].header['DATE-LOC'].split('T')[0] not in dates):
-            dates.append(file[0].header['DATE-LOC'].split('T')[0])
-        '''
+def getData():
+    for directory in os.listdir('./fits'):
+        if os.path.isdir(f"fits/{directory}"):
+            print(directory)
+            for file in os.listdir(f"fits/{directory}"):
+                filename = file
+                if 'tmp' not in file and '.fits' in file:
+                    if file == '.DS_Store':
+                        continue          
+                    file = fits.open(f"./fits/{directory}/{file}")
+                    date = file[0].header['DATE-OBS'].split('T')[0]
+                    if date not in dates:
+                        dates[date] = {
+                            "pictures": 1,
+                            "telescopeUsed": file[0].header['TELESCOP'],
+                            "dates": [file[0].header['DATE-OBS']]
+                        }
+                    else:
+                        dates[date]['pictures'] += 1
+                        dates[date]['dates'].append(file[0].header['DATE-OBS'])
+                        if(dates[date]['telescopeUsed'] != file[0].header['TELESCOP']):
+                            print('telescope mismatch')
 
 
-print(dates)
 
-for date, value in dates.items():
-    print(date)
-    lowestTime = {"value": 24, "string": None}
-    highestTime = {"value": 0, "string": None}
-    for iso in value['dates']:
-        time = iso.split('T')[1].split('-')
-        hour = time[0]
-        minute = time[1]
-        second = time[2]
-        time = int(hour) + int(minute)/60 + float(second)/3600
-        print(time)
-        if time > highestTime['value']:
-            highestTime['string'] = f"{hour}:{minute}:{second}"
-            highestTime['value'] = time
-        elif time < lowestTime['value']:
-            lowestTime['string'] = f"{hour}:{minute}:{second}"
-            lowestTime['value'] = time
-    dates[date]['lowestTime'] = lowestTime
-    dates[date]['highestTime'] = highestTime
-    del dates[date]['dates']
+    for date, value in dates.items():
+        lowestTime = {"value": 24, "string": None}
+        highestTime = {"value": 0, "string": None}
+        for iso in value['dates']:
+            time = iso.split('T')[1].split('-')
+            hour = time[0]
+            minute = time[1]
+            second = time[2]
+            time = int(hour) + int(minute)/60 + float(second)/3600
+            print(time)
+            if time > highestTime['value']:
+                highestTime['string'] = f"{hour}:{minute}:{second}"
+                highestTime['value'] = time
+            elif time < lowestTime['value']:
+                lowestTime['string'] = f"{hour}:{minute}:{second}"
+                lowestTime['value'] = time
+        dates[date]['lowestTime'] = lowestTime
+        dates[date]['highestTime'] = highestTime
+        del dates[date]['dates']
 
-print(dates)
+    print(dates)
+
+getData()
